e3640cc6c4471e230eee920c57fcfb4d7aea57d1
zahid_97 <zahid.khan@wissen.com>
2022-04-28 18:03:49 +0530

    in-memory cache

--- src/main/java/com/driver/DriverClass.java
+++ src/main/java/com/driver/DriverClass.java
@@ -5,26 +5,35 @@ import com.inMemoryCache.Cache.Cache;
 
 public class DriverClass {
 	
-	static class TestClass{
-		
-		private void TestCache() throws InterruptedException {
-
-			Cache<String, String> cache = new Cache<>(2, 3);
+	static class TestClass implements Runnable{
+		Cache<String, String> cache = new Cache<>(2, 3);
+		@Override
+		public void run() {
 			cache.put("1st", "Test1");
 			cache.put("2nd", "Test2");
 			cache.put("3rd", "Test3");
-			System.out.println(cache.get("1st"));
+			System.out.println(Thread.currentThread().getName() + " " +  cache.get("1st"));
 			cache.put("4th", "Test4");
-			System.out.println(cache.get("1st"));
-			Thread.sleep(3000);
-			System.out.println(cache.get("2nd"));
+			System.out.println(Thread.currentThread().getName() + " " +  cache.get("1st"));
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + " " + cache.get("2nd"));
+			
 		}
 		
 	}
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		TestClass testClass = new TestClass();
-		testClass.TestCache();
+		Thread t1 = new Thread(testClass);
+		Thread t2 = new Thread(testClass);
+		
+		t1.start();
+		t2.start();
 	}
 	
 	
